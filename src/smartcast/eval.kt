37b75515bb2cf76a878386c2d975721fe93c2736
bbelovic <bbelovic@gmail.com>
2018-09-17 21:45:36 +0200

    extending eval with when expression

--- src/smartcast/eval.kt
+++ src/smartcast/eval.kt
@@ -1,11 +1,22 @@
 package smartcast
 
-fun eval(expression: Expression): Int {
+fun eval(expression: Expression): Int =
     if (expression is Num) {
-        return expression.value;
+        expression.value
+    } else if (expression is Sum) {
+        eval(expression.right) + eval(expression.left)
+    } else {
+        throw IllegalArgumentException("Unknown expression")
     }
-    if (expression is Sum) {
-        return eval(expression.right) + (expression.left)
-    }
-    throw IllegalArgumentException("Unknown expression");
+
+fun evalWithWhen(expression: Expression): Int =
+        when (expression) {
+            is Num -> expression.value
+            is Sum -> evalWithWhen(expression.left) + evalWithWhen(expression.right)
+            else -> throw IllegalArgumentException("unknown expression")
+        }
+
+fun main(array: Array<String>) {
+    val result = eval(Sum(Num(1), Num(4)))
+    println("Result: $result")
 }
