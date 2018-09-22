c8438a95eb1024cff550f7f6e46c2d6db77c7c53
bbelovic <bbelovic@gmail.com>
2018-09-22 18:55:09 +0200

    polishing extension functions

--- src/functions/jointostring.kt
+++ src/functions/jointostring.kt
@@ -1,9 +1,9 @@
 package functions
 
-fun <T> joinToString(c: Collection<T>, separator: String = "; ", prefix: String = "{", postfix: String = "}"): String {
+fun <T> Collection<T>.joinToString(separator: String = "; ", prefix: String = "{", postfix: String = "}"): String {
     val result = StringBuilder()
     result.append(prefix)
-    for ((index, element) in c.withIndex()) {
+    for ((index, element) in withIndex()) {
         if (index > 0) result.append(separator)
         result.append(element)
     }
@@ -11,10 +11,16 @@ fun <T> joinToString(c: Collection<T>, separator: String = "; ", prefix: String 
     return result.toString()
 }
 
+fun String.lastChar(): Char = get(length - 1);
+fun String.firstChar(): Char = get(0);
+
 fun main(args: Array<String>) {
     val c = listOf("one", "two", "three")
-    println(joinToString(c, ";", "[", "]"))
+    println(c.joinToString(";", "[", "]"))
     val empty: List<String> = emptyList()
-    println(joinToString(empty, ";", prefix = "[", postfix = "]"))
-    println(joinToString(c,  prefix = "[", postfix = "]", separator =  "# "))
+    println(empty.joinToString(";", prefix = "[", postfix = "]"))
+    println(c.joinToString(prefix = "[", postfix = "]", separator =  "# "))
+
+    println("Boris".lastChar())
+    println("Boris".firstChar())
 }
