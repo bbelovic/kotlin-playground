31d28b298ed2622315b6834e3117c0b952580302
bbelovic <bbelovic@gmail.com>
2018-11-08 21:33:33 +0100

    Delegated properties, part 4

--- src/main/kotlin/conventions/delegation/Person.kt
+++ src/main/kotlin/conventions/delegation/Person.kt
@@ -1,6 +1,11 @@
 package conventions.delegation
 
+import kotlin.properties.Delegates
+import kotlin.reflect.KProperty
+
 class Person(val username: String, age: Int, salary: Int) : PropertyChangeAware() {
-    var age: Int by ObservableProperty(age, propertyChangeSupport)
-    var salary: Int by ObservableProperty(salary, propertyChangeSupport)
+    val delegate = {prop: KProperty<*>, oldValue: Int, newValue: Int ->
+        println("'${prop.name}' changed from [$oldValue] to [$newValue]")}
+    var age: Int by Delegates.observable(age, delegate)
+    var salary: Int by Delegates.observable(salary, delegate)
 }
--- src/test/kotlin/conventions/delegation/PersonTest.kt
+++ src/test/kotlin/conventions/delegation/PersonTest.kt
@@ -1,19 +1,13 @@
 package conventions.delegation
 
 import org.junit.jupiter.api.Test
-import java.beans.PropertyChangeEvent
-import java.beans.PropertyChangeListener
 
 internal class PersonTest {
     @Test
     fun should_trigger_listener_on_property_change() {
         val person = Person("boris", 33, 80_000)
-        person.addPropertyChangeListener(
-                PropertyChangeListener { evt: PropertyChangeEvent? ->
-                    println("Property [${evt?.propertyName}] changed from " +
-                            "[${evt?.oldValue}] to [${evt?.newValue}]") }
-        )
 
+        person.age = 18
         person.salary = 10_000
     }
 }
