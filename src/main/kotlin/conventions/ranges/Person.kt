c3d59d5d0740eff798a4e7c67827cf9882b30d98
bbelovic <bbelovic@gmail.com>
2018-11-17 11:40:30 +0100

    Destructuring conventions

--- src/main/kotlin/conventions/ranges/Person.kt
+++ src/main/kotlin/conventions/ranges/Person.kt
@@ -1,7 +1,12 @@
 package conventions.ranges
 
-class Person(val age: Int) : Comparable<Person> {
+class Person(val login: String, val age: Int) : Comparable<Person> {
     override fun compareTo(other: Person): Int {
         return age.compareTo(other.age)
+    }
+
+    operator fun component1() = this.login
+    operator fun component2(): Int {
+        return this.age
     }
 }
--- src/test/kotlin/conventions/ranges/ComparisonsTest.kt
+++ src/test/kotlin/conventions/ranges/ComparisonsTest.kt
@@ -2,16 +2,27 @@ package conventions.ranges
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import java.util.*
 
 class ComparisonsTest {
     @Test
     fun should_compare_persons() {
-        val p1 = Person(18)
-        val p2 = Person(40)
-        val p3 = Person(40)
-        val p4 = Person(99)
+        val p1 = Person("john.doe", 18)
+        val p2 = Person("jane.doe", 40)
+        val p3 = Person("pepa", 40)
+        val p4 = Person("franta", 99)
         Assertions.assertTrue(p1 < p2)
         Assertions.assertTrue(p3 <= p2)
         Assertions.assertTrue(p4 > p2)
+    }
+
+    @Test
+    fun should_parse_person_from_string() {
+        val (login, age) = parsePerson("jdoe:19")
+    }
+
+    fun parsePerson(input: String?): Person {
+        val split = input?.split(":") ?: listOf("", "-1")
+        return Person(split[0], split[1].toInt())
     }
 }
