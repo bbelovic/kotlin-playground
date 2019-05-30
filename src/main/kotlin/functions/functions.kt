4389fe848f22a9385a0fdefdd0298e19481c6bb9
bbelovic <bbelovic@gmail.com>
2019-05-30 20:42:20 +0200

    exercise high order functions

--- src/main/kotlin/functions/functions.kt
+++ src/main/kotlin/functions/functions.kt
@@ -3,7 +3,12 @@ package functions
 val square: (Int) -> Int = {it * it}
 val square2 = {x: Int -> x * x}
 
+val printSquare5: ((Int) -> Int) -> Unit = { println(it(5)) }
+
 fun main() {
     println(square(5))
     println(square2(5))
+
+    printSquare5 {it * it}
+    printSquare5 {it + it}
 }
